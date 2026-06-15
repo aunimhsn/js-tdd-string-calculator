af325ce7f4a44bfcc12bda908429713e5c18e035
Aunim HASSAN <aunim.hassan@gmail.com>
2026-06-15 11:39:53 +0200

    Convert split string elements to numbers

--- app/js/001.js
+++ app/js/001.js
@@ -4,7 +4,7 @@ export class StringCalculator {
         if (str.length === 0) return 0;
         if (! isNaN(str)) return parseInt(str);
 
-        let numbers = str.split(',')
+        let numbers = str.split(',').map(n => Number(n))
         return numbers[0] + numbers[1];
     }
 
